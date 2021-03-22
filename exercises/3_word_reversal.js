8f3feb41e3368a275769cc6c1966bd1bd411cc2c
k <klavyatur@yahoo.com>
2021-03-22 02:29:15 -0400

    #3 complete

--- exercises/3_word_reversal.js
+++ exercises/3_word_reversal.js
@@ -9,7 +9,14 @@ let statement = 'Penn Mutual sells life insurance'
 
 function reverseLetters(str) {
   // PLACE YOUR CODE BELOW
+  let splitStr = str.split(' ');
 
+  // this is probably not optimal, but it works...
+  for(let i = 0; i < splitStr.length; i++) {
+    splitStr[i] = splitStr[i].split('').reverse().join('');
+  }
+
+  return splitStr.join(' ');
   // PLACE YOUR CODE ABOVE
 }
 
